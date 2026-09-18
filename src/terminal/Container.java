ef8612340c49dbb36b008ed8108c4f74dea48fb4
Laboratorio Informática 3 - Pasto <labinf3.pasto@CAMPUS.local>
2026-09-18 16:06:16 -0500

    rwer

--- src/terminal/Container.java
+++ src/terminal/Container.java
@@ -3,24 +3,38 @@ public class Container implements TerminalFactory (){
 	private int rate;
 	private int imbalance;
 	private float openwork;
+	private boolean loadingKnwoledge;
+	private boolean humidityCertificate;
 
-	public Container (String loadingEquipment, int rate, int imbalance, float openwork){
+	public Container (String loadingEquipment, int rate, int imbalance, float openwork, boolean loadingKnowledge, boolean humidtyCertificate){
 		this.loadingEquipment = loadingEquipment;
 		this.rate = rate;
 		this.imbalance = imbalance;
-		this.openwork = openwork
+		this.openwork = openwork;
+		this.loadingKnowledge = loadingKnowledge;
+		this.loadingEquipment = loadingEquipment;
 	}
 
 	@Override
 	public void createLoadTeam(){
-		this.loadignEquipment = "GRANEL_SOLIDO"
+		this.loadignEquipment = "CONTENEDORES"
 	}
 
 	@Overrride 
 	public boolean createStabilityValidator(){
 		if (this.imbalance <= 8 && this.openwork <= 1.8){
-			return true
+			return True;
+		}else{
+			return False;
 		}
 	}
+
+	@Override
+	public boolean createStabilityValidator(){
+		if (this.loadingKnowledge == True && this.loadingEquipment == True ){
+			return True;
+		}
+		return False
+	}    
 
 }
